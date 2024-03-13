2bed2680444f8a8e71e76ab7c4d13d86172c59f3
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-03-13 19:49:24 +0100

    create new module using intellij

---
+++ nos-intelli/src/main/java/com/emat/nosintelli/NosIntelliApp.java
@@ -0,0 +1,7 @@
+package com.emat.nosintelli;
+
+public class NosIntelliApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World from nos intelli module!");
+    }
+}
